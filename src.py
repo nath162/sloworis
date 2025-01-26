5a6cc8d110ddfa260865e8b48e7e0773c9fbd704
Nath162 <lopibite@gmail.com>
2025-01-26 17:30:57 +0100

    added more lisibility to the parser

--- src.py
+++ src.py
@@ -4,10 +4,14 @@ import argparse
 
 parser = argparse.ArgumentParser(
         prog = 'sloworis',
-        description = 'my version of slowloris'
+        usage = 'Slowloris DDOs technique',
+        description = 'my version of slowloris',
+        prefix ='--',
+        argument_default = None,
+        exit_on_error = True
         )
 
-parser.add_argument('-u',action='store_const',default=None)
+parser.add_argument('--u',action='store_const',default=None)
 
-url = parser.parse_args(['-u'])
+url = parser.parse_args(['--u'])
 print(url)
